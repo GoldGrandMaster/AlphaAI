f558a1e90705e17e2a5bd0c57117d74a8567e5a6
passioneffort <agronkercishta.ai@gmail.com>
2023-07-22 09:12:48 -0400

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,17 +5,17 @@ import {Link} from 'react-router-dom';
 const home = () => {
   return (
     <>
-      <div className='hr_container'>
-        <div className='hr_content'>
-          <div className='home22-intro-content'>
-            <h1 className='home22-intro-title'>Skills speak louder than words</h1>
-          </div>
-          <div>
-            <p className='home22-intro-text'>We help companies develop the strongest tech teams around. We help candidates sharpen their tech skills and pursue job opportunities.</p>
-            <p className='home22-intro-sign'>
+      <div className='home22-intro'>
+        
+        <div className='home22-intro-content'>
+          <h1 className='home22-intro-title'>Skills speak louder than words</h1>
+          <div className='home22-intro-text'>
+            <p>We help companies develop the strongest tech teams around. We help candidates sharpen their tech skills and pursue job opportunities.</p>
+            <p>
               <Link className='signup' to = {"/signup"}>Sign up</Link>
               <Link className = 'requestdemo' to = {"/"}>Request demo</Link>
             </p>
+            <p>Over 40% of developers worldwide and 3,000 companies use HackerRank</p>
           </div>
         </div>
       </div>
