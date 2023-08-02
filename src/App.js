ecc2d99e82b5f2e52db51eebedbc73642631203b
passioneffort <agronkercishta.ai@gmail.com>
2023-08-02 12:15:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Header } from "./Layout";
-import Home from "./Pages/Home";
-import './styles/app.scss'
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import { Header } from "./Layout";
+// import Home from "./Pages/Home";
+// import './styles/app.scss'
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path = "/" element = {<Home />} />
-      </Routes>
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path = "/" element = {<Home />} />
+//       </Routes>
       
-    </Router>
-  );
-}
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
