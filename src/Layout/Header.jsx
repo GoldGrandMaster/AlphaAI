7b1e0204a7eaa7266fff9a95c036086fa208d086
passioneffort <agronkercishta.ai@gmail.com>
2023-08-04 12:06:17 -0400

    modify

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -4,26 +4,26 @@ import hackrank from "../assets/hackerrank_logo.png"
 
 import "../styles/header.scss"
 
-// const header = () => {
-//   return (
-//     <nav>
-//       <main>
-//         <img width = "170" src = {hackrank} alt = 'Graphics' />
-//         <div>
-//           <Link to = {"/"}>Products</Link>
-//           <Link to = {"/"}>Solutions</Link>
-//           <Link to = {"/"}>Resources</Link>
-//           <Link to = {"/"}>Pricing</Link>
-//         </div>
-//         <div>
-//           <Link to = "/">For candidates</Link>
-//           |
-//           <Link className = 'requestdemo' to = {"/"}>Request demo</Link>
-//           <Link className='signup' to = {"/signup"}>Sign up</Link>
-//         </div>
-//       </main>
-//     </nav>
-//   )
-// }
+const header = () => {
+  return (
+    <nav>
+      <main>
+        <img width = "170" src = {hackrank} alt = 'Graphics' />
+        <div>
+          <Link to = {"/"}>Products</Link>
+          <Link to = {"/"}>Solutions</Link>
+          <Link to = {"/"}>Resources</Link>
+          <Link to = {"/"}>Pricing</Link>
+        </div>
+        <div>
+          <Link to = "/">For candidates</Link>
+          |
+          <Link className = 'requestdemo' to = {"/"}>Request demo</Link>
+          <Link className='signup' to = {"/signup"}>Sign up</Link>
+        </div>
+      </main>
+    </nav>
+  )
+}
 
 export default header
